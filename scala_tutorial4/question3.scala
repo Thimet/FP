fb9d1807b6f8bf954a2f3ac5812eb6b84d80c10e
Thimet <92925044+Thimet@users.noreply.github.com>
2022-08-01 22:24:02 +0530

    Update question3.scala

--- scala_tutorial4/question3.scala
+++ scala_tutorial4/question3.scala
@@ -12,9 +12,8 @@ object question3 extends App{
     def formatNames(name: String, f:(String) => String): String = {
         f(name);
     }
-
-    println(formatNames("Benny", toUpper));
-    println(formatNames("Niroshan", toUpper));
-    println(formatNames("Saman", toLower));
-    println(formatNames("Kumara", toLower));
+    println(formatNames("Benny", toUpper))
+    println(formatNames("Niroshan".substring(0,2), toUpper)+formatNames("Niroshan".substring(2), toLower))
+    println(formatNames("Saman", toLower))
+    println(formatNames("Kumara".substring(0,1), toUpper)+formatNames("Kumara".substring(1,5), toLower)+formatNames("Kumara".substring(5), toUpper))
 }
